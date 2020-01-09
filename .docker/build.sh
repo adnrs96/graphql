071692740f581447c67a2b7d20c68b1948c836e0
Aurelien ARINO <aurelien.arino@gmail.com>
2020-01-09 18:58:06 +0100

    ci: add multiple ci workflows

---
+++ .docker/build.sh
@@ -0,0 +1,23 @@
+BRANCH=$(jq --raw-output .ref "${GITHUB_EVENT_PATH}");
+BRANCH=$(echo "${BRANCH/refs\/heads\//}")
+BRANCH="$(echo "$BRANCH" | sed -r 's/\//_/g')"
+NAME="storyscript/platform-graphql"
+
+docker build \
+  -t $NAME:$BRANCH \
+  .
+
+if [ $BRANCH = 'master' ]; then
+  docker tag $NAME:$HASH $NAME:latest
+  echo "Successfully tagged $NAME:latest"
+  echo ""
+  echo "Pushing $NAME:latest"
+  docker push $NAME:latest
+fi
+
+echo ""
+echo "Pushing $NAME:$BRANCH"
+docker push $NAME:$BRANCH
+echo ""
+echo "Pushing $NAME:$BRANCH"
+docker push $NAME:$BRANCH
