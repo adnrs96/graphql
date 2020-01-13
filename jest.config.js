5fb961cdf44cf36ddde21324397c638ebc0f844f
Jean Barriere <jean@barriere.io>
2020-01-13 15:52:39 +0100

    chore(tests): update docker environment to run tests properly + add supertest

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/(index|postgraphile/StoryscriptPlugin).ts'],
   testPathIgnorePatterns: ['node_modules', 'dist'],
   preset: 'ts-jest',
   testMatch: null
